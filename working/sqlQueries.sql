c37a7c74accc8528afcc86570dcbf0f9ee83da03
ec14192 <p.s.pettersen@se14.ac.uk>
2016-03-22 13:43:31 +0000

    Listed the data we are adding to the DB and a list of names with customers

---
+++ working/sqlQueries.sql
@@ -0,0 +1,22 @@
+#Add 3-4 view definitions
+
+
+
+#Add 10-12 complex queries 
+
+
+#Sample test data
+100 rooms:
+ - 33 superior(Price: 170, capability: 3 ), 33 deluxe(Price: 200, capability: 2) and 34 basic(Price: 100, capability: 2).
+ - room numbers in range 0-25 for 4 floors (eg. 103, 203, 303, 403)
+ - room availability 50/50
+ 
+ 50 bookings:
+ - 40 Checkin dates from today and 1 year ahead.
+ - 10 checkin dates from before today for testing
+
+ 20 customers:
+ - 
+
+ 70 guests:
+ - Different names and agegroups.
